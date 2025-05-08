c6e87c21a31b4bf9ffdec938197843aa1a9bca90
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-05-07 20:44:25 -0700

    Update

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,12 +25,6 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
-        
-        # Bullet Settings
-        self.bullet_speed = 2.0
-        self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
     
     def run_game(self):
         """Star the main loop for the game."""

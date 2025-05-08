e84cda8a32718a1e7ab2e85f7021ec1b632163bc
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-05-07 19:39:06 -0700

    updates for keypresses

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,6 +16,10 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
+        # NOTE: Uncomment for FullScreen
+        # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -45,6 +49,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         """Respond to Key Releases."""

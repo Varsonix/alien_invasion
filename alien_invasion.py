f5fd56f3dfebb3e8ac4547c82d32a672e27ce5e6
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-05-07 20:12:38 -0700

    Implement bullet class and init

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,6 +23,12 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        
+        # Bullet Settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
     
     def run_game(self):
         """Star the main loop for the game."""

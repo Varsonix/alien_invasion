1b6de47a6a5693e4610005cab8e657736b173cef
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2025-05-15 18:22:16 -0700

    updates to alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,6 +37,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()           
             self.clock.tick(60)
 
@@ -108,6 +109,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
